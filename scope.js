c72a62c45fcc559e7b41f081af5131d587210a97
AjayAsnani <ajaysnani555@gmail.com>
2024-05-01 00:32:38 +0530

    new commit

---
+++ scope.js
@@ -0,0 +1,12 @@
+{
+  let a = 10;
+  console.log("Let has Functional Scope", a);
+}
+{
+  var b = 20;
+}
+console.log("Var has Global Scope", b);
+{
+  const c = 30;
+  console.log("Const has Functional Scope", c);
+}
